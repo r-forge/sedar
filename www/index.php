53ebb8ff74c5c8a91d6895aecf3f8a5a2d3faf51
sdray (Stéphane Dray) <dray@biomserv.univ-lyon1.fr>
2008-06-02 14:48:28 +0000

    add a link to the workshop website
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sedar@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,7 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p> This project is born at a <a href="http://biomserv.univ-lyon1.fr/~dray/sedar/">workshop</a> organized in Lyon.</p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
