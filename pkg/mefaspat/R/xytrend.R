bb8de046ee09b6ccacd9b7e211176c035445d4ee
psolymos (Peter Solymos) <solymos@ualberta.ca>
2009-05-25 05:28:19 +0000

    modifications to be
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sedar@73 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mefaspat/R/xytrend.R
+++ pkg/mefaspat/R/xytrend.R
@@ -10,3 +10,21 @@ function(xy)
     colnames(out) <- c("x","y","x2","x.y","y2","x3","x2.y","x.y2","y3")
     return(out)
 }
+
+#`xytrend` <-
+#function(x, y = NULL, degree = 3)
+#{
+#    
+#    if (is.null(y) && NCOL(x) == 2) {
+#        y <- scale(x[,2])
+#        x <- scale(x[,1])
+#    } else {
+#        if (NCOL(x) > 2)
+#            stop("x can have max 2 columns")
+#        x <- scale(x)
+#        y <- scale(y)
+#    }
+#    out <- data.frame(x,y,x^2,x*y,y^2,x^3,x^2*y,x*y^2,y^3)
+#    colnames(out) <- c("x","y","x2","x.y","y2","x3","x2.y","x.y2","y3")
+#    out
+#}
