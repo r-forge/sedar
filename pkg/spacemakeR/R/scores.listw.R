556c6470533b13ca25713ac82d6d5be927d6bc82
sdray (Stéphane Dray) <dray@biomserv.univ-lyon1.fr>
2010-05-21 15:32:06 +0000

    new argument MEM.autocor
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sedar@83 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/spacemakeR/R/scores.listw.R
+++ pkg/spacemakeR/R/scores.listw.R
@@ -1,13 +1,16 @@
 "scores.listw" <-
-function(listw,echo=FALSE) {
+  function(listw,echo=FALSE, MEM.autocor = c("all","positive", "negative")) {
     if (!inherits(listw, "listw")) 
-          stop("not a listw object")
+      stop("not a listw object")
+    MEM.autocor <- match.arg(MEM.autocor)
     w <- listw2mat(listw)
+    sumW <- sum(w)
+    n <- nrow(w)
     symmetric <- isSymmetric.matrix(w,check.attributes=FALSE)
     if (symmetric==FALSE){
-        cat (paste("listw not symmetric, (w+t(w)) used in the place of w","\n\n"))
-        w=(w+t(w))/2
-        }
+      cat (paste("listw not symmetric, (w+t(w)) used in the place of w","\n\n"))
+      w=(w+t(w))/2
+    }
     
     row.mean <- apply(w, 1, mean)
     col.mean <- apply(w, 2, mean)
@@ -15,7 +18,7 @@ function(listw,echo=FALSE) {
     w <- sweep(w, 1, row.mean)
     w <- sweep(w, 2, col.mean)
     w <- w + tot.mean
-    # a voir w=w/max(w)
+    ## a voir w=w/max(w)
     
     res <- eigen(w,EISPACK=TRUE,symmetric=TRUE)
     eq0 <- apply(as.matrix(res$values/max(abs(res$values))),1,function(x) identical(all.equal(x, 0), TRUE))
@@ -23,7 +26,17 @@ function(listw,echo=FALSE) {
     if(echo) {cat(paste("vector number",which(eq0),"corresponding to null eigenvalue is removed","\n"))}
     res$values <- res$values[-which(eq0)]
     res$vectors <- res$vectors[,-which(eq0)]
+    if(MEM.autocor == "positive"){
+      
+      posi <- which(res$values > -sumW/((n-1)*n))
+      res$values <- res$values[posi]
+      res$vectors <- res$vectors[,posi]
+    }
+    if( MEM.autocor == "negative"){
+      neg <- sort(which(res$values < -sumW/((n-1)*n)), decreasing = TRUE)
+      res$values <- res$values[neg]
+      res$vectors <- res$vectors[,neg]
+    }  
     res$call=match.call()
     res
-    
-}
+  }
--- pkg/spacemakeR/R/test.W.R
+++ pkg/spacemakeR/R/test.W.R
@@ -1,32 +1,35 @@
 "test.W" <-
-function(Y,nb,xy,f=NULL,...){
+  function(Y,nb,xy,MEM.autocor = c("all","positive", "negative"),f=NULL, ...){
     mycall <- pairlist(...)
     res <- list()
+    MEM.autocor <- match.arg(MEM.autocor)
     if(!(is.null(f))){
-        nbdist <- nbdists(nb,as.matrix(xy))
-        if(!(is.null(mycall))){
-            param <- expand.grid(as.list(mycall))
-            m1 <- match(names(param),names(formals(f)))
-            for (i in 1:nrow(param)){
-                formals(f)[m1] <- unclass(param[i,])
-                res[[i]] <- scores.listw(nb2listw(nb,style="B",glist=lapply(nbdist,f)))
-            }
+      nbdist <- nbdists(nb,as.matrix(xy))
+      if(!(is.null(mycall))){
+        param <- expand.grid(as.list(mycall))
+        m1 <- match(names(param),names(formals(f)))
+        for (i in 1:nrow(param)){
+          formals(f)[m1] <- unclass(param[i,])
+          res[[i]] <- scores.listw(nb2listw(nb,style="B",glist=lapply(nbdist,f)), MEM.autocor = MEM.autocor)
         }
-        else {res[[1]] <- scores.listw(nb2listw(nb,style="B",glist=lapply(nbdist,f)))}
+      }
+      else {res[[1]] <- scores.listw(nb2listw(nb,style="B",glist=lapply(nbdist,f)), MEM.autocor = MEM.autocor)}
     }
-    else { res[[1]] <- scores.listw(nb2listw(nb,style="B")) }
+    else { res[[1]] <- scores.listw(nb2listw(nb,style="B"), MEM.autocor = MEM.autocor) }
     res2 <- lapply(res,function(x) ortho.AIC(Y=Y,X=x$vec, ord.var=TRUE))
     if(!(is.null(mycall))){
-        res3 <- data.frame(AICc=unlist(lapply(res2,function(x) min(x[[1]],na.rm=TRUE))),NbVar= unlist(lapply(res2,function(x) which.min(x[[1]]))))
-        res3 <- cbind(param,res3)
+      res3 <- data.frame(AICc=unlist(lapply(res2,function(x) min(x[[1]],na.rm=TRUE))),NbVar= unlist(lapply(res2,function(x) which.min(x[[1]]))))
+      res3 <- cbind(param,res3)
+    }
+    else{
+      res3 <- data.frame(AICc=unlist(lapply(res2,function(x) min(x[[1]],na.rm=TRUE))),NbVar= unlist(lapply(res2,function(x) which.min(x[[1]]))))
+    }
 
-        }
-    else{ res3 <- data.frame(AICc=unlist(lapply(res2,function(x) min(x[[1]],na.rm=TRUE))),NbVar= unlist(lapply(res2,function(x) which.min(x[[1]]))))}
-   
-    cat ("\n\nBest model:\n\n\n")
     thebest <- which.min(res3$AICc)
+    cat (paste("\n\nAICc for the null model:",res2[[thebest]]$AICc0,"\n"))
+    cat ("\nBest spatial model:\n")
     print(res3[thebest,])
-
+    
     return(list(all=res3,best=c(res[[thebest]],res2[[thebest]])))
     
 }
