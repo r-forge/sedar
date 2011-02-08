68c6b557279aafcb6fa70b1e91ffc3582c7cd4fd
jarioksa (Jari Oksanen) <jari.oksanen@oulu.fi>
2011-02-08 19:56:59 +0000

    adjust dot size for plot area and use full scale of the colour palette
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sedar@89 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/metapop/R/plot.metacapa.R
+++ pkg/metapop/R/plot.metacapa.R
@@ -6,8 +6,12 @@
     opal <- palette()
     on.exit(opal)
     palette(heat.colors(255))
-    cl <-  (1 - x$x)*254 + 1
-    plot(locations, asp=1, cex=sqrt(x$A*10), xlab="", ylab="", pch=21,
+    cl <-  (1 - x$x/max(x$x))*254 + 1
+    ## Find adjustment for size: largest circle is 1/25 of plot range
+    ran <- max(diff(apply(locations, 2, range)))
+    dia <- sqrt(max(x$A))
+    adj <- ran/dia/25
+    plot(locations, asp=1, cex=sqrt(x$A*adj), xlab="", ylab="", pch=21,
          col="blue", bg=cl, ...)
     invisible(locations)
 }
