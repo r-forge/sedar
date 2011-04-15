c810c7cc251e97f2569703ef28121342bced7728
jarioksa (Jari Oksanen) <jari.oksanen@oulu.fi>
2011-04-15 17:40:13 +0000

    implement as.matrix for landgame results
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sedar@93 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/hubbell/R/as.matrix.landscape.R
@@ -0,0 +1,16 @@
+### Changes 'landgame' result to a community data frame where each
+### site is a row, and each distinct species is a column
+as.matrix.landscape <-
+    function(x, ...)
+{
+    spec <- unique(x)
+    nr <- prod(dim(x)[1:2])
+    dim(x) <- c(nr, dim(x)[3])
+    df <- matrix(0, nr, length(spec))
+    colnames(df) <- spec
+    for(i in 1:nrow(x)) {
+        tbl <- table(x[i,])
+        df[i, names(tbl)] <- tbl
+    }
+    as.data.frame(df)
+}
